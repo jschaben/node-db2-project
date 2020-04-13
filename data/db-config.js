7c78a959605b9866b730b10911e819c8ed38e7df
Josh schaben <jschaben42@gmail.com>
2020-04-13 16:27:45 -0500

    setting up

---
+++ data/db-config.js
@@ -0,0 +1,8 @@
+const knex = require("knex");
+const knexfile = require("../knexfile");
+
+const environment = process.env.NODE_ENV || "development";
+
+const knexConfig = knexfile[environment];
+
+module.exports = knex(knexConfig);
