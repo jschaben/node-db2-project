2f95fd3fd841e2ba03b1131bd6ec3151ecdc9927
Josh schaben <jschaben42@gmail.com>
2020-04-13 16:59:48 -0500

    seed file for stretch

--- data/migrations/20200413161924_cars-schema.js
+++ data/migrations/20200413161924_cars-schema.js
@@ -2,17 +2,11 @@
 exports.up = function(knex) {
   return knex.schema.createTable("cars", (tbl) => {
     tbl.increments();
-
     tbl.string("VIN", 128).notNullable().unique().index();
-
     tbl.string("make", 128).notNullable();
-
     tbl.string("model", 128).notNullable();
-
     tbl.string("mileage", 128).notNullable();
-
     tbl.string("transmission", 128);
-
     tbl.boolean("title");
   });
 };
