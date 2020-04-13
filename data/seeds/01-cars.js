1b0efbd29fea7e190ffe77ad2507d96f9af360fa
Josh schaben <jschaben42@gmail.com>
2020-04-13 17:03:03 -0500

    mvp and stretch

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('cars').truncate()
+  return knex("cars").del()
     .then(function () {
       // Inserts seed entries
-      return knex('cars').insert([
+      return knex("cars").insert([
         {
           id: 1,
           VIN: "0123456",
